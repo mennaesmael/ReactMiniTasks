527d019891ab74f7b89796e1e12bffc581cead75
Mennaesmael <114106408+mennaesmael@users.noreply.github.com>
2023-10-26 23:52:13 +0200

    Trying props vs state

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,6 +1,11 @@
 import ListGroup from "./Components/ListGroup";
+
 function App() {
- return<div><ListGroup></ListGroup></div>;
+    const handleSelectItem= (item:string) => {
+        console.log(item);
+    }
+    let items = [" Cairo", "Fauom", "Aswan", "Giza", "Alex"];
+ return<div><ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}></ListGroup></div>;
 }
 
 export default App;
--- react-app/src/Components/ListGroup.tsx
+++ react-app/src/Components/ListGroup.tsx
@@ -1,15 +1,34 @@
-
-function ListGroup() {
+import { useState } from "react";
+interface props{
+ items: string [];
+ heading: string;
+ onSelectItem: (item:string)=> void;
+}
+function ListGroup({items,heading , onSelectItem}:props) {
+ 
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
-    <h1>List Group</h1>
-        <ul className="list-group">
-          <li className="list-group-item">An item</li>
-          <li className="list-group-item">A second item</li>
-          <li className="list-group-item">A third item</li>
-          <li className="list-group-item">A ourth item</li>
-          <li className="list-group-item">And a fifth one</li>
-        </ul>
+      <h1>{heading}</h1>
+      <ul className="list-group">
+        {items.length === 0 && <p>NO ITEMS</p>}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
