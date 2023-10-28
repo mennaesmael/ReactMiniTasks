920dcef3272d9975417f54bdc2a44a5b810ec7dc
Mennaesmael <114106408+mennaesmael@users.noreply.github.com>
2023-10-28 05:48:51 +0200

    Passing children via PROPS

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,11 +1,14 @@
-import ListGroup from "./Components/ListGroup";
+
+import Alert from "./Components/Alert";
 
 function App() {
-    const handleSelectItem= (item:string) => {
-        console.log(item);
-    }
-    let items = [" Cairo", "Fauom", "Aswan", "Giza", "Alex"];
- return<div><ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}></ListGroup></div>;
+    
+   
+ return<div>
+    <Alert>
+    Hello <span>World</span>
+    </Alert>
+    </div>;
 }
 
 export default App;
